0822b7859ec5a570c61cb4fc1d628ff19cf40828
Pérez Sanz <rubenprzsnz@gmail.com>
2025-04-03 10:30:00 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -163,17 +163,6 @@
 
 				});
 
-				.panel({
-				    delay: 500,
-				    hideOnClick: false,
-				    hideOnSwipe: false,
-				    resetScroll: true,
-				    resetForms: true,
-				    side: 'left',
-				    target: $body,
-				    visibleClass: 'is-menu-visible'
-				});
-
 		// Scroll lock.
 		// Note: If you do anything to change the height of the sidebar's content, be sure to
 		// trigger 'resize.sidebar-lock' on $window so stuff doesn't get out of sync.
